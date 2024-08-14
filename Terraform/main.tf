45506a45c64af5d95c3411d24e412c3d60aff474
cloudtechtrainer <126149128+cloudtechtrainer@users.noreply.github.com>
2024-08-14 23:45:23 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -28,7 +28,7 @@ resource "aws_security_group" "ssh" {
 
 # Create a new EC2 instance and associate it with the security group
 resource "aws_instance" "example" {
-  count = 4 
+  count = 2
   ami           = "ami-05b5693ff73bc6f84"
   instance_type = "t3.micro"
   key_name = "jenkin-key"
