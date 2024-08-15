402f75d2ac4998aa95f6db25adad1983ef40bfe2
cloudtechtrainer <126149128+cloudtechtrainer@users.noreply.github.com>
2024-08-15 20:06:02 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -24,7 +24,14 @@ resource "aws_security_group" "ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
